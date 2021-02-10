38c96fd51a697667d4554b9c8ea1cbcf22ccfef2
Dv <dv@Dvs-MacBook-Pro.local>
2021-02-11 01:47:56 +0530

    new changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,8 +9,31 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="section-center nav-center">
-        <img src={logo} alt="logo" />
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <ul className="nav-links">
+            <li>
+              <a href="/" className="nav-link">
+                Features
+              </a>
+            </li>
+            <li>
+              <a href="/" className="nav-link">
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a href="/" className="nav-link">
+                Resources
+              </a>
+            </li>
+          </ul>
+        </div>
         <GoThreeBars className="toggle" onClick={toggle} />
+        <div className="nav-btns">
+          <button className="nav-btn">login</button>
+          <button className="nav-btn sign-btn">sign up</button>
+        </div>
       </div>
     </nav>
   );
